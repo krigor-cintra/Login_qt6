2247328e29116d0465925e886f2e849d0a37dab4
krigor-cintra <krigor.gcs@gmail.com>
2022-08-04 06:54:22 -0300

    0.0.1
    Conexão com banco de dados, interface e criação das funções.

---
+++ bancodedados.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+try:
+
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="admin",
+        database="programa1"
+    )
+except mysql.connector.Error as erro:
+    print(erro)
+
+mycursor = mydb.cursor()
+
+def login_creat(email,senha):
+    mycursor.execute("insert into login (email , senha) values('%s','%s')"),email,senha
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+
+
+
+def login(email,senha):
+    mycursor.execute(f"SELECT email from programa1.login  where email like '{email}'")
+    result=mycursor.fetchall()
+    if result != []:
+        mycursor.execute(f"SELECT senha0 from programa1.login  where email like '{email}'")
+        result = mycursor.fetchall()
+        if str(result[0]==senha):
+            print("login sucess")
---
+++ validação_email.py
@@ -0,0 +1,8 @@
+def email(email):
+    email=email
+    print(email)
+def senha(senha):
+    senha=senha
+    print(senha)
+
+
